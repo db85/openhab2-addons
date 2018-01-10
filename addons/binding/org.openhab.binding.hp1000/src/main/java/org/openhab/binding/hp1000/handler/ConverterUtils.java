4ab4a9dfd228cc8e8b8b0a14c0cecca66030125f
Daniel Bauer <daniel.bauer@intive.com>
2018-01-10 12:33:53 +0100

    basic channel update

---
+++ addons/binding/org.openhab.binding.hp1000/src/main/java/org/openhab/binding/hp1000/handler/ConverterUtils.java
@@ -0,0 +1,32 @@
+package org.openhab.binding.hp1000.handler;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.StringType;
+import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
+
+public final class ConverterUtils {
+
+    @Nullable
+    public static Double parseDouble(String value) {
+        if (StringUtils.isEmpty(value)) {
+            return null;
+        }
+
+        return Double.valueOf(value);
+    }
+
+    public static State toDecimalType(@Nullable Double value) {
+        return (value == null) ? UnDefType.NULL : new DecimalType(value);
+    }
+
+    public static State toStringType(@Nullable String value) {
+        return (value == null) ? UnDefType.NULL : new StringType(value);
+    }
+
+    private ConverterUtils() {
+        // utils class
+    }
+}
