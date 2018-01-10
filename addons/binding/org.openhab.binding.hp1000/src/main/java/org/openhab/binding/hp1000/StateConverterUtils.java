406585061d4b29d62fb2270e42ea80cccd6afc0f
Daniel Bauer <daniel.bauer@intive.com>
2018-01-10 14:02:24 +0100

    license header

--- addons/binding/org.openhab.binding.hp1000/src/main/java/org/openhab/binding/hp1000/StateConverterUtils.java
+++ addons/binding/org.openhab.binding.hp1000/src/main/java/org/openhab/binding/hp1000/StateConverterUtils.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.hp1000;
 
 import org.apache.commons.lang.StringUtils;
@@ -27,6 +35,6 @@ public final class StateConverterUtils {
     }
 
     private StateConverterUtils() {
-        // utils class
+        // helper class
     }
 }
